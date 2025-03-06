81a39546bce0447d47bec1545a1005e1a8857be2
Tiffany Andriamiharimanana <tiffany.a@vffice.com>
2025-03-06 09:53:58 -0500

    card component

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src="https://i.pinimg.com/236x/63/e8/72/63e87243b841d70bb83743f17f8d8a98.jpg" alt="profile picture"></img>
+            <h2 className="card-title">Tiffany</h2>
+            <p className="card-text">I am a student at Concordia University and play the guitar</p>
+        </div>
+    );
+}
+
+export default Card
