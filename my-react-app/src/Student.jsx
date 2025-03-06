6424949d949a05c502c0ff8d52120418e8433b62
Tiffany Andriamiharimanana <tiffany.a@vffice.com>
2025-03-06 13:23:14 -0500

    student component with props

--- my-react-app/src/Student.jsx
+++ my-react-app/src/Student.jsx
@@ -1,3 +1,6 @@
+
+import PropTypes from 'prop-types'
+
 function Student(props) {
     return(
         <div className="student">
@@ -6,6 +9,18 @@ function Student(props) {
             <p>Student: {props.isStudent ? "Yes" : "No"}</p>
         </div>
     );
+}
+
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool,
+}
+
+Student.defaultProps = {
+    name: "Guest",
+    age: 0,
+    isStudent: false,
 }
 
 export default Student
