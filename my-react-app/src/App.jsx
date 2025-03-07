48aeaa5af9f06dedf889b22392127f2b553cadbc
Tiffany Andriamiharimanana <tiffany.a@vffice.com>
2025-03-07 09:35:43 -0500

    List component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,12 +2,13 @@ import Card from "./Card.jsx";
 import Button from "./Button.jsx";
 import Student from "./Student.jsx";
 import UserGreeting from "./UserGreeting.jsx";
+import List from "./List.jsx"
 
 function App() {
 
   return (
     <>
-    <UserGreeting isLoggedIn = {true}/>
+    
     </>
   );
   
